e8f185b309833f52ed2b183ca845e7f3e7566c81
Omid <soheilnia.o@entekhabelectronic.com>
2023-09-26 10:00:46 +0330

    cookies added

--- components/index.js
+++ components/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import AboutMe from "./about-me";
 import MyExperiences from "./experiences";
 import { checkIpAPI } from "../api";
+import Cookies from "js-cookie";
 
 export default function MyResume() {
   const [lang, setLang] = useState("IR");
@@ -12,8 +13,10 @@ export default function MyResume() {
       .then(({ data }) => {
         if (data.success == true && data.country == "Iran") {
           setLang("IR");
+          Cookies.set("user-lang", "IR");
         } else if (data.success == true && data.country !== "Iran") {
           setLang("EN");
+          Cookies.set("user-lang", "EN");
         }
       })
       .catch((e) => console.log(e));
